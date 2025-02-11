1eb136101b4b5acd064f8a6af903aaa330b34ab2
vidyashreek853 <vidyashreek853@gmail.com>
2025-02-10 21:51:05 -0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,4 @@
+document.querySelector("button").addEventListener("click", function () {
+    let searchQuery = document.querySelector("input").value;
+    alert("Searching for: " + searchQuery);
+});
